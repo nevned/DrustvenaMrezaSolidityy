3737ac3d6615a9d6b42c1b3707627beacf7e6b9d
TaraMarkovic <taramarkovic6@gmail.com>
2024-09-27 18:01:16 +0200

    First Network commit

---
+++ contracts/SocialNetwork.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+contract SocialNetwork{
+    mapping(address=>string) private statuses;
+    address public owner;
+    event StatusUpdated(address  user, string newStatus);
+
+    constructor() {
+        owner=msg.sender;
+    }
+
+
+    function setStatus(string calldata NewStatus) external{
+
+    statuses[msg.sender]=NewStatus;
+    emit StatusUpdated(msg.sender, NewStatus);
+
+    }
+
+    function getStatus(address user) external view returns(string memory){
+        return statuses[user];
+    }
+
+
+
+}
